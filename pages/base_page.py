abb55b3e57c984ea52d2f7094c179b74a2d57d44
smoozmy <smoozmy@yandex.ru>
2025-05-11 10:18:55 +0300

    Создан base_page, добавлены общие методы

---
+++ pages/base_page.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__(self, driver, timeout=10):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, timeout)
+
+    def open(self, url):
+        self.driver.get(url)
+
+    def click(self, locator):
+        self.wait.until(EC.element_to_be_clickable(locator)).click()
+
+    def fill(self, locator, text):
+        self.wait.until(EC.visibility_of_element_located(locator)).send_keys(text)
+
+    def get_text(self, locator):
+        return self.wait.until(EC.visibility_of_element_located(locator)).text
+
+    def is_visible(self, locator):
+        return self.wait.until(EC.visibility_of_element_located(locator))
+
+    def is_clickable(self, locator):
+        return self.wait.until(EC.element_to_be_clickable(locator))
+
+    def current_url(self):
+        return self.driver.current_url
+
+    def get_element(self, locator):
+        return self.wait.until(EC.presence_of_element_located(locator))
